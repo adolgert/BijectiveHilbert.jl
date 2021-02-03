8ec5bee92edbca13b2ec8778f976c7e613822a94
Drew Dolgert <adolgert@uw.edu>
2021-02-02 18:13:53 -0800

    adding tests for simple2d and found that Int8 can't hold two 4-bit values, which I knew

--- test/test_hamilton.jl
+++ test/test_hamilton.jl
@@ -529,7 +529,7 @@ decode_hilbert!(gg, Y, h)
 end
 
 
-@safetestset compact_type_interactions = "Compact type interactions" begin
+@safetestset spacegray_type_interactions = "SpaceGray type interactions" begin
     using BijectiveHilbert
     using UnitTestDesign
     using Random
@@ -601,4 +601,55 @@ for idx in Base.IteratorsMD.CartesianIndices((length(aas), length(tts)))
     hl2 = encode_hilbert_zero(gg, X)
     @test hl2 == hli
 end
+end
+
+
+@safetestset compact_type_interactions = "Compact type interactions" begin
+    using BijectiveHilbert
+    using UnitTestDesign
+    using Random
+    rng = Random.MersenneTwister(9790323)
+    for retrial in 1:5
+        AxisTypes = shuffle(rng, [Int8, Int, UInt, Int128, UInt8, UInt128])
+        IndexTypes = shuffle(rng, [Union{}, Int8, UInt8, Int, UInt, Int128, UInt128])
+        Count= shuffle(rng, [0, 1])
+        Dims = shuffle(rng, [2, 3, 4])
+        Bits = shuffle(rng, [2, 3, 4, 5])
+        test_set = all_pairs(
+            AxisTypes, IndexTypes, Count, Dims, Bits;
+        )
+        for (A, I, C, D, B) in test_set
+            if I == Union{}
+                gg = Compact(fill(B, D))
+                I = index_type(gg)
+            else
+                gg = Compact(I, fill(B, D))
+            end
+            if B * D > log2(typemax(I))
+                continue
+            end
+            last = (one(I) << (B * D)) - one(I) + I(C)
+            mid = one(I) << (B * D - 1)
+            few = 5
+            X = zeros(A, D)
+            hlarr = vcat(C:min(mid, few), max(mid + 1, last - few):last)
+            for hl in hlarr
+                hli = I(hl)
+                if C == 0
+                    decode_hilbert_zero!(gg, X, hli)
+                    hl2 = encode_hilbert_zero(gg, X)
+                    if hl2 != hli
+                        @show A, I, C, D, B, X
+                        @test hl2 == hli
+                    end
+                    @test typeof(hl2) == typeof(hli)
+                else
+                    decode_hilbert!(gg, X, hli)
+                    hl3 = encode_hilbert(gg, X)
+                    @test hl3 == hli
+                    @test typeof(hl3) == typeof(hli)
+                end
+            end
+        end
+    end
 end
